d49b378a9a837cb2f2e38b581db7aeb2d8b6f37c
Oskar Kärrman <05k4r@live.se>
2013-09-19 14:03:28 +0200

    Added javadoc for interfaces, modified functions

--- Project/src/se/chalmers/krogkollen/map/IMapView.java
+++ Project/src/se/chalmers/krogkollen/map/IMapView.java
@@ -2,6 +2,12 @@ package se.chalmers.krogkollen.map;
 
 import se.chalmers.krogkollen.IView;
 
+/**
+ * Interface for a MapView object
+ * 
+ * @author Oskar Kärrman
+ *
+ */
 public interface IMapView extends IView {
 	
 }
--- Project/src/se/chalmers/krogkollen/pub/IPub.java
+++ Project/src/se/chalmers/krogkollen/pub/IPub.java
@@ -1,16 +1,41 @@
 package se.chalmers.krogkollen.pub;
 
+/**
+ * Interface for a Pub object
+ * 
+ * @author Oskar Kärrman
+ *
+ */
 public interface IPub {
+	
+	/**
+	 * @return the name of the pub
+	 */
 	public String getName();
 	
+	/**
+	 * @return the description of the pub
+	 */
 	public String getDescription();
 	
+	/**
+	 * @return the opening hours of the pub
+	 */
 	public String getOpeningHours();
 	
+	/**
+	 * @return the age restriction of the pub
+	 */
 	public int getAgeRestriction();
 	
+	/**
+	 * @return the current queue time of the pub
+	 */
 	public int getQueueTime();
 	
+	/**
+	 * @return the timestamp of when the queue time was last updated
+	 */
 	public int getQueueTimeLastUpdatedTimestamp();
 	
 	//public Point getCoordinates(); // TODO how to handle coordinates? sync with Google maps

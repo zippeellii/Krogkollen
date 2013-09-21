8854ed8321a546ba7f7389b20c20f476d6921fe2
linneaotterlind <linnea.otterlind@gmail.com>
2013-09-21 14:28:55 +0200

    Added new class for calculating the users position

--- Project/src/se/chalmers/krogkollen/map/MapActivity.java
+++ Project/src/se/chalmers/krogkollen/map/MapActivity.java
@@ -1,19 +1,26 @@
 package se.chalmers.krogkollen.map;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import se.chalmers.krogkollen.R;
 import se.chalmers.krogkollen.R.layout;
 import se.chalmers.krogkollen.R.menu;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class MapActivity extends Activity {
 
     private GoogleMap mMap;
+    private UserLocation userLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,9 @@ public class MapActivity extends Activity {
 		setContentView(R.layout.activity_map);
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         addMarker();
+        this.userLocation = new UserLocation((LocationManager) this.getSystemService(Context.LOCATION_SERVICE));
+        addUserMarker(this.userLocation.getCurrentLocation());
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(this.userLocation.getCurrentLatLng(), 15, 0, 0)));
 	}
 
 	@Override
@@ -29,7 +39,13 @@ public class MapActivity extends Activity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
-
+	
+	private void addUserMarker(Location location){
+		mMap.addMarker(new MarkerOptions()
+						.position(new LatLng(location.getLatitude(), location.getLongitude()))
+						.title("User"));
+	}
+	
     private void addMarker() {
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {

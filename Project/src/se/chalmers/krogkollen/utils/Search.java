5103db01d9c765ea2e3da83d4730986a620d9126
linneaotterlind <linnea.otterlind@gmail.com>
2013-09-24 13:05:31 +0200

    Added search class in the util package with very temporary search method.

---
+++ Project/src/se/chalmers/krogkollen/utils/Search.java
@@ -0,0 +1,40 @@
+package se.chalmers.krogkollen.utils;
+
+import se.chalmers.krogkollen.pub.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+
+	private static Search instance = null;
+	
+	private Search() {
+		
+	}
+	
+	public Search getInstance() {
+		if(instance == null) {
+			instance = new Search();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Returns a list with all pubs that have names that contains the search string. (Temporary)
+	 * 
+	 * @param search	the string that will be search for in all the pubs names.
+	 * @return			a list of pubs
+	 */
+	public List<IPub> search(String search) {
+		List<IPub> allPubs = PubUtilities.getInstance().getPubList();
+		
+		List<IPub> matches = new ArrayList<IPub>();
+		for(IPub pub: allPubs) {
+			if(pub.getName().toLowerCase().contains(search)){
+				matches.add(pub);
+			}
+		}
+		return matches;
+	}
+	
+}

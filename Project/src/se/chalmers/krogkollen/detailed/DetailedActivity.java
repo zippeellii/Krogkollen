8bbf3a32252a8fda792d81119edab483aace1fd5
Per Thoresson <pthoresson@gmail.com>
2013-09-25 15:45:16 +0200

    onOurWay

---
+++ Project/src/se/chalmers/krogkollen/detailed/DetailedActivity.java
@@ -0,0 +1,17 @@
+package se.chalmers.krogkollen.detailed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: perthoresson
+ * Date: 2013-09-25
+ * Time: 10:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class DetailedActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

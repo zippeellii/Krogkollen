1daa1f8626ecc2e5ecd3e29f79d6f5c77dfb4b77
Johan Backman <backman@hotmail.se>
2013-09-27 10:10:38 +0200

    added test fiolder

---
+++ Project/src/tests/UserLocationTest.java
@@ -0,0 +1,59 @@
+package tests;
+
+/**
+ * UserLocationTest (UTF-8)
+ * <p/>
+ * Author: Johan Backman
+ * Date: 2013-09-27
+ */
+
+public class UserLocationTest {
+
+    public void setUp() throws Exception {
+
+    }
+
+    public void testGetCurrentLocation() throws Exception {
+
+    }
+
+    public void testGetCurrentLatLng() throws Exception {
+
+    }
+
+    public void testStopTrackingUser() throws Exception {
+
+    }
+
+    public void testStartTrackingUser() throws Exception {
+
+    }
+
+    public void testOnLocationChanged() throws Exception {
+
+    }
+
+    public void testOnProviderDisabled() throws Exception {
+
+    }
+
+    public void testOnProviderEnabled() throws Exception {
+
+    }
+
+    public void testOnStatusChanged() throws Exception {
+
+    }
+
+    public void testIsBetterLocation() throws Exception {
+
+    }
+
+    public void testAddObserver() throws Exception {
+
+    }
+
+    public void testRemoveObserver() throws Exception {
+
+    }
+}

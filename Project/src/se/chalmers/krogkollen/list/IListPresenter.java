15266356314eb90e24a4724fc77be69632eaa412
Albin Garpetun <albin.garpetun@gmail.com>
2013-10-04 11:44:08 +0200

    Added skeleton for listview.

---
+++ Project/src/se/chalmers/krogkollen/list/IListPresenter.java
@@ -0,0 +1,10 @@
+package se.chalmers.krogkollen.list;
+
+/**
+ * Interface for the ListPresenter.
+ *
+ * @author Albin Garpetun
+ *         Created 2013-09-22
+ */
+public interface IListPresenter {
+}

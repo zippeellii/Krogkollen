33247f7662e2b2b20bafda151f367770b7fac935
Filip Carlen <filipcarlen@gmail.com>
2013-10-04 15:44:02 +0200

    Javadoc in DetailedPresenter

--- Project/src/se/chalmers/krogkollen/detailed/DetailedPresenter.java
+++ Project/src/se/chalmers/krogkollen/detailed/DetailedPresenter.java
@@ -10,22 +10,27 @@ import se.chalmers.krogkollen.pub.PubUtilities;
 
 /**
  * A presenter class for the detailed view of a pub
- * 
- * @author Oskar Karrman
- *
  */
 public class DetailedPresenter implements IDetailedPresenter {
 
+    /** The view connected with the Presenter */
 	private DetailedActivity view;
+
+    /** The pub that the Presenter holds */
 	private IPub pub;
 
 	@Override
 	public void setView(IView view) {
-
 		this.view= (DetailedActivity)view;
 
 	}
 
+    /**
+     * Sets the pub and updates it locally
+     * @param pubID The pubs ID
+     * @throws NoBackendAccessException
+     * @throws NotFoundInBackendException
+     */
 	public void setPub(String pubID) throws NoBackendAccessException, NotFoundInBackendException{
 		pub = PubUtilities.getInstance().getPub(pubID);
 		Backend.getInstance().updatePubLocally(pub);
@@ -108,18 +113,31 @@ public class DetailedPresenter implements IDetailedPresenter {
 
 	}
 
+    /**
+     * Saves the state of the favorite locally
+     */
     public void saveFavoriteState(){
         SharedPreferences.Editor editor = view.getSharedPreferences(pub.getID(), 0).edit();
         editor.putBoolean("star", !(view.getSharedPreferences(pub.getID(), 0).getBoolean("star", true)));
         editor.commit();
     }
 
+    /**
+     *
+     * Saves the state of the thumb because a user is only allowed to vote 1 time.
+     * @param thumb
+     */
 	public void saveThumbState(int thumb){
 		SharedPreferences.Editor editor = view.getSharedPreferences(pub.getID(), 0).edit();
 		editor.putInt("thumb", thumb);
 		editor.commit();
 	}
 
+    /**
+     * Converts the hour string
+     * @param hour
+     * @return
+     */
 	public String convertOpeningHours(int hour){
 		if(hour / 10 ==0){
 			return "0"+hour;
@@ -127,28 +145,50 @@ public class DetailedPresenter implements IDetailedPresenter {
 		return ""+hour;
 	}
 
+    /**
+     * Sends the queue time to the view
+     */
 	public void getQueueTime(){
 		view.updateQueueIndicator(pub.getQueueTime());
 	}
 
+    /**
+     * Sends the name, description, opening hours, age restriction and entrance fee to the view
+     */
 	public void getText(){
 		view.updateText(pub.getName(), pub.getDescription(), convertOpeningHours(pub.getTodaysOpeningHour()) + " - " +
 				(convertOpeningHours(pub.getTodaysClosingHour())), ""+pub.getAgeRestriction() + " År", ""+pub.getEntranceFee()
 				+ " :-");
 	}
 
+    /**
+     * Tells the view whether the thumb down or the thumb up is clicked.
+     */
 	public void getThumbs(){
 		view.setThumbs(view.getSharedPreferences(pub.getID(), 0).getInt("thumb", 0));
 	}
 
+    /**
+     * Tells the view weather the star is clicked or not.
+     */
     public void getFavoriteStar(){
         view.updateStar(view.getSharedPreferences(pub.getID(), 0).getBoolean("star", true));
     }
 
+    /**
+     * Sends the current rating to the view.
+     * @throws NoBackendAccessException
+     * @throws NotFoundInBackendException
+     */
 	public void getVotes() throws NoBackendAccessException, NotFoundInBackendException{
 		view.updateVotes(""+ pub.getPositiveRating(), ""+ pub.getNegativeRating());
 	}
 
+    /**
+     * Updates the info of a pub
+     * @throws NoBackendAccessException
+     * @throws NotFoundInBackendException
+     */
     public void updateInfo() throws NoBackendAccessException, NotFoundInBackendException{
         Backend.getInstance().updatePubLocally(pub);
     }

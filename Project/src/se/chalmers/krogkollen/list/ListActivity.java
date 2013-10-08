b5f0687facc88fa74a021a34e49a8feb61b9e946
Filip Carlen <filipcarlen@gmail.com>
2013-10-08 10:50:16 +0200

    More functionality to ListActivity

--- Project/src/se/chalmers/krogkollen/list/ListActivity.java
+++ Project/src/se/chalmers/krogkollen/list/ListActivity.java
@@ -6,25 +6,23 @@ import android.view.View;
 import android.widget.ListView;
 import se.chalmers.krogkollen.R;
 import se.chalmers.krogkollen.pub.IPub;
+import se.chalmers.krogkollen.pub.PubUtilities;
+
 
 /**
  * Activity for the list view. This shows a list that is sorted by a few default values, that the user can chose between.
  * Such as distance, queue-time or favorites.
  *
- * @author Albin Garpetun
- *         Created 2013-09-22
  */
 public class ListActivity extends Activity implements IListView {
 
     private ListView listView;
+    private IPub[] pub_data;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        IPub pub_data[] = new IPub[]
-                {
-                        FILL DIS SHIT WITH A LOT OF PUBS
-                };
+        pub_data = (IPub[]) PubUtilities.getInstance().getPubList().toArray();
 
         PubListAdapter adapter = new PubListAdapter(this,
                 R.layout.listview_item, pub_data);
@@ -37,6 +35,8 @@ public class ListActivity extends Activity implements IListView {
 
         listView.setAdapter(adapter);
     }
+
+
 
 	@Override
 	public void navigate(Class<?> destination) {

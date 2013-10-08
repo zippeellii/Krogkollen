64a4f2294320022f94be3a6ef245d55614a4e829
Filip Carlen <filipcarlen@gmail.com>
2013-10-08 15:37:55 +0200

    Added tab to listview

--- Project/src/se/chalmers/krogkollen/list/ListActivity.java
+++ Project/src/se/chalmers/krogkollen/list/ListActivity.java
@@ -1,12 +1,17 @@
 package se.chalmers.krogkollen.list;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.ListView;
 import se.chalmers.krogkollen.R;
 import se.chalmers.krogkollen.pub.IPub;
 import se.chalmers.krogkollen.pub.PubUtilities;
+
+import java.util.List;
 
 
 /**
@@ -14,7 +19,7 @@ import se.chalmers.krogkollen.pub.PubUtilities;
  * Such as distance, queue-time or favorites.
  *
  */
-public class ListActivity extends Activity implements IListView {
+public class ListActivity extends FragmentActivity implements IListView, ActionBar.TabListener{
 
     private ListView listView;
     private IPub[] pub_data;
@@ -22,10 +27,15 @@ public class ListActivity extends Activity implements IListView {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
         presenter = new ListPresenter();
         presenter.setView(this);
 
-        pub_data = (IPub[]) PubUtilities.getInstance().getPubList().toArray();
+        List<IPub> pubs = PubUtilities.getInstance().getPubList();
+        pub_data = new IPub[pubs.size()];
+        for(int i = 0; i < pubs.size(); i++){
+            pub_data[i] = pubs.get(i);
+        }
 
         PubListAdapter adapter = new PubListAdapter(this,
                 R.layout.listview_item, pub_data);
@@ -73,4 +83,19 @@ public class ListActivity extends Activity implements IListView {
 		// TODO Auto-generated method stub
 		
 	}
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }

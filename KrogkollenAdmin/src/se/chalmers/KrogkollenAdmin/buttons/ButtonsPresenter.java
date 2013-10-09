03a46c42f834d31c6820f685a2380a973ae1d1d5
Albin Garpetun <albin.garpetun@gmail.com>
2013-10-09 10:37:03 +0200

    Cleanup in KrogkollenAdmin and replaced some occurences of Foobar with Krogkollen

--- KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/buttons/ButtonsPresenter.java
+++ KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/buttons/ButtonsPresenter.java
@@ -14,6 +14,23 @@ import se.chalmers.KrogkollenAdmin.main.MainActivity;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/*
+ * This file is part of Krogkollen.
+ *
+ * Krogkollen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krogkollen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krogkollen.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * The presenter class for the ButtonsActivity. It holds all the data and logic for the view.
  *
--- KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/main/MainPresenter.java
+++ KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/main/MainPresenter.java
@@ -14,6 +14,23 @@ import se.chalmers.KrogkollenAdmin.buttons.ButtonsPresenter;
 
 import java.util.List;
 
+/*
+ * This file is part of Krogkollen.
+ *
+ * Krogkollen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krogkollen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krogkollen.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * The presenter class for the MainActivity. It holds all the data and logic for the view.
  *

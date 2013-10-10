342b90463569597ef60eb3ffab1378ffb2c334b1
Filip Carlen <filipcarlen@gmail.com>
2013-10-10 16:01:53 +0200

    merge.. :D

--- Project/src/se/chalmers/krogkollen/list/QueuetimeFragment.java
+++ Project/src/se/chalmers/krogkollen/list/QueuetimeFragment.java
@@ -6,22 +6,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import se.chalmers.krogkollen.pub.IPub;
 import se.chalmers.krogkollen.search.SortByQueueTime;
 
 public class QueuetimeFragment extends ListFragment {
+
+    private PubListAdapter adapter;
+    private ListModel model;
 
 	
 	   @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
 
-           ListModel model = new ListModel();
+           model = new ListModel();
            View rootView = inflater.inflate(R.layout.fragment_queue_list, container, false);
 
-           PubListAdapter adapter = new PubListAdapter(getActivity(), R.layout.listview_item, model.getSortedArray(new SortByQueueTime()));
+           adapter = new PubListAdapter(getActivity(), R.layout.listview_item, model.getSortedArray(new SortByQueueTime()));
 
            setListAdapter(adapter);
 
            return rootView;
 	    }
+
+    public void favoriteStarHandler(View view){
+        IPub selectedPub = (IPub) view.getTag();
+        //TODO vad skall hända?
+    }
 }

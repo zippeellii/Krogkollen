a9e071ad7e5f39bf32926efa06a0c5a2b367b354
Albin Garpetun <albin.garpetun@gmail.com>
2013-10-11 16:05:21 +0200

    What?

--- KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/buttons/Timers.java
+++ KrogkollenAdmin/src/se/chalmers/KrogkollenAdmin/buttons/Timers.java
@@ -5,4 +5,5 @@ package se.chalmers.KrogkollenAdmin.buttons;
  *         Created 2013-09-22
  */
 public class Timers {
+    // WILL MOVE TIMRERS HERE AND MAKE THEM SINGLETON TO AVOID BUG WHERE MULTIPLE TIMERS START.
 }

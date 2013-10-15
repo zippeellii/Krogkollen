3d14b8f4ab14d9ea9258c9cc72a5ce43f6f5bea9
Filip Carlen <filipcarlen@gmail.com>
2013-10-15 15:37:28 +0200

    Fixed so SortedListFragment is the only availible fragment.

--- Project/src/se/chalmers/krogkollen/adapter/TabsPagerAdapter.java
+++ Project/src/se/chalmers/krogkollen/adapter/TabsPagerAdapter.java
@@ -1,39 +1,41 @@
 package se.chalmers.krogkollen.adapter;
 
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import se.chalmers.krogkollen.list.*;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import se.chalmers.krogkollen.sort.SortByDistance;
-import se.chalmers.krogkollen.sort.SortByQueueTime;
 
 public class TabsPagerAdapter extends FragmentPagerAdapter {
-	
-    private IListPresenter presenter;
+
+    public static String SORT_MODE = "SORT_MODE";
  
     // TODO javadoc
-    public TabsPagerAdapter(FragmentManager fm, IListPresenter presenter) {
+    public TabsPagerAdapter(FragmentManager fm) {
         super(fm);
-        this.presenter = presenter;
     }
  
     @Override
     public ListFragment getItem(int index) {
+
+        ListFragment fragment = new SortedListFragment();
+        Bundle bundle = new Bundle();
  
         switch (index) {
         case 0:
-            // Top Rated fragment activity
-            return new SortedListFragment(new SortByQueueTime(), presenter);
+            bundle.putInt(SORT_MODE, 0);
+            break;
         case 1:
-            // Games fragment activity
-            return new SortedListFragment(new SortByDistance(), presenter);
+            bundle.putInt(SORT_MODE, 1);
+            break;
         case 2:
-            // Movies fragment activity
-            return new SortedListFragment(new SortByDistance(), presenter);
+            bundle.putInt(SORT_MODE, 2);
+            break;
         }
- 
-        return null;
+
+        fragment.setArguments(bundle);
+        return fragment;
     }
  
     @Override
--- Project/src/se/chalmers/krogkollen/list/SortedListFragment.java
+++ Project/src/se/chalmers/krogkollen/list/SortedListFragment.java
@@ -5,9 +5,15 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import se.chalmers.krogkollen.R;
+import se.chalmers.krogkollen.adapter.TabsPagerAdapter;
+import se.chalmers.krogkollen.detailed.DetailedActivity;
+import se.chalmers.krogkollen.map.MapPresenter;
 import se.chalmers.krogkollen.pub.IPub;
 import se.chalmers.krogkollen.sort.ISort;
+import se.chalmers.krogkollen.sort.SortByDistance;
+import se.chalmers.krogkollen.sort.SortByQueueTime;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,28 +26,57 @@ public class SortedListFragment extends ListFragment {
 
 
     private IPub[] data;
-    private IListPresenter presenter;
-
-    public SortedListFragment(ISort sort, IListPresenter presenter){
-        this.presenter = presenter;
-        data = presenter.sortList(sort);
-    }
-
+    private PubListAdapter adapter;
+    private ISort sort;
+    private ListModel model;
+    private IListView view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        model = new ListModel();
+        data = setSortMode(getArguments().getInt(TabsPagerAdapter.SORT_MODE));
         View rootView = inflater.inflate(R.layout.fragment_listview, container, false);
 
-        PubListAdapter adapter = new PubListAdapter(getActivity(), R.layout.listview_item, data);
+        adapter = new PubListAdapter(getActivity(), R.layout.listview_item, data);
+
+        System.out.println(getActivity() instanceof ListActivity);
+
+        view = (ListActivity) getActivity();
 
         setListAdapter(adapter);
 
-        getListView().setOnItemClickListener(presenter);
-
         return rootView;
     }
+
+
+    public IPub[] setSortMode(int sortMode){
+        switch (sortMode){
+
+            case 0:
+                sort = new SortByQueueTime();
+                break;
+
+            case 1:
+                sort = new SortByDistance();
+                break;
+
+            case 2:
+                sort = new SortByDistance();
+                break;
+        }
+
+        return model.getSortedArray(sort);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Bundle bundle = new Bundle();
+        bundle.putString(MapPresenter.MAP_PRESENTER_KEY, data[position].getID());
+        view.navigate(DetailedActivity.class, bundle);
+    }
+
 
 }
 

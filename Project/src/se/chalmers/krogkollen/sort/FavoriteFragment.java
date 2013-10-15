64b9ec7abf99bbdda0e4d808acb6515fb928376b
zippeellii <jonathan.nilsfors@gmail.com>
2013-10-15 12:35:04 +0200

    Added a singleton class to make preferences global

---
+++ Project/src/se/chalmers/krogkollen/sort/FavoriteFragment.java
@@ -0,0 +1,7 @@
+package se.chalmers.krogkollen.sort;
+
+
+
+public class FavoriteFragment{
+
+}
---
+++ Project/src/se/chalmers/krogkollen/utils/Preferences.java
@@ -0,0 +1,51 @@
+package se.chalmers.krogkollen.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+/**
+ * A singleton class for handling saved preferences
+ * @author Jonathan Nilsfors
+ *
+ */
+public class Preferences {
+
+	private static Preferences prefs;
+	private Context context;
+	private SharedPreferences sharedPrefs;
+	
+	//Private constructor to prevent duplicated instantiation
+	private Preferences(){
+	}
+	
+	public Preferences getInstance(){
+		if(this.prefs == null){
+			prefs = new Preferences();
+		}
+		return prefs;
+	}
+	/**
+	 * Initializes the Preferences singleton
+	 * @param context 
+	 */
+	public void init(Context context){
+		this.context = context;
+		this.sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.context);
+	}
+	/**
+	 * Saves the preference
+	 * @param id the id of the preference
+	 * @param value the value of the preference
+	 */
+	public void savePreference(String id, boolean value){
+		this.sharedPrefs.edit().putBoolean(id, value).commit();
+	}
+	/**
+	 * Loads the preference value for specified id
+	 * @param id the requested objects id
+	 * @return true or false
+	 */
+	public boolean loadPreference(String id){
+		return this.sharedPrefs.getBoolean(id, false);
+	}
+}

b2d1ab1cde7893aebc393b9352e8f0574009c717
Johan Backman <backman@hotmail.se>
2013-10-17 14:24:01 +0200

    Add new icons.

--- Project/src/se/chalmers/krogkollen/help/HelpActivity.java
+++ Project/src/se/chalmers/krogkollen/help/HelpActivity.java
@@ -13,8 +13,8 @@ import se.chalmers.krogkollen.map.MapActivity;
 import se.chalmers.krogkollen.utils.Constants;
 
 /**
- * Activity for the help screen
- * 
+ * Activity for the help screen.
+ *
  * @author Johan Backman 
  * Date: 2013-10-02
  */
@@ -23,38 +23,38 @@ public class HelpActivity extends Activity {
     private String previousActivityKey;
 
     @Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         previousActivityKey = getIntent().getStringExtra(Constants.ACTIVITY_FROM);
 
-		setContentView(R.layout.activity_help);
-	}
+        setContentView(R.layout.activity_help);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.help, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.help, menu);
 
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setIcon(R.drawable.transparent_spacer);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setIcon(R.drawable.transparent_spacer);
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 NavUtils.navigateUpTo(this, new Intent(this,
                         previousActivityKey.equalsIgnoreCase(Constants.LIST_ACTIVITY_NAME) ?
                                 ListActivity.class : MapActivity.class));
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }

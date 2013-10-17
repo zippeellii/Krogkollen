22c7dd3a9b9995e9ba661fa5f7a956c35ee2eba3
Oskar Kärrman <05k4r@live.se>
2013-10-17 14:23:15 +0200

    Code checked for release

--- Project/src/se/chalmers/krogkollen/backend/NotFoundInBackendException.java
+++ Project/src/se/chalmers/krogkollen/backend/NotFoundInBackendException.java
@@ -1,11 +1,26 @@
 package se.chalmers.krogkollen.backend;
 
+/*
+ * This file is part of Krogkollen.
+ *
+ * Krogkollen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krogkollen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krogkollen.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * An exception indicating that a object which was supposed to be in the backend cannot be found
- * there
  * 
  * @author Oskar Karrman
- * 
  */
 public class NotFoundInBackendException extends Exception {
 

5b9ad62303acc8e908b19cfa361efdf25fc5625b
Oskar Kärrman <05k4r@live.se>
2013-10-18 12:41:03 +0200

    Code checked for release

--- Project/src/se/chalmers/krogkollen/adapter/TabsPagerAdapter.java
+++ Project/src/se/chalmers/krogkollen/adapter/TabsPagerAdapter.java
@@ -1,53 +1,73 @@
 package se.chalmers.krogkollen.adapter;
 
-
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import se.chalmers.krogkollen.list.*;
+import se.chalmers.krogkollen.utils.Constants;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-// TODO javadoc
+/*
+ * This file is part of Krogkollen.
+ *
+ * Krogkollen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Krogkollen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Krogkollen.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * An adapter class for fragment handling in the list view
+ */
 public class TabsPagerAdapter extends FragmentPagerAdapter {
 
-    public static final String SORT_MODE = "SORT_MODE";
- 
-    // TODO javadoc
-    public TabsPagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
- 
-    @Override
-    public ListFragment getItem(int index) {
+	/**
+	 * Creates a new TabsPagerAdapter
+	 * 
+	 * @param fm
+	 */
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
 
-        ListFragment fragment = new SortedListFragment();
-        Bundle bundle = new Bundle();
- 
-        switch (index) {
-        case 0:
-            bundle.putInt(SORT_MODE, 0);
-            break;
-        case 1:
-            bundle.putInt(SORT_MODE, 1);
-            break;
-        case 2:
-            bundle.putInt(SORT_MODE, 2);
-            break;
-        }
+	@Override
+	public ListFragment getItem(int index) {
 
-        fragment.setArguments(bundle);
-        return fragment;
-    }
- 
-    @Override
-    public int getCount() {
-        // get item count - equal to number of tabs
-        return 3;
-    }
+		ListFragment fragment = new SortedListFragment();
+		Bundle bundle = new Bundle();
 
-    @Override
-    public int getItemPosition(Object object) {
-        return POSITION_NONE;
-    }
- 
+		switch (index) {
+			case 0:
+				bundle.putInt(Constants.SORT_MODE, 0);
+				break;
+			case 1:
+				bundle.putInt(Constants.SORT_MODE, 1);
+				break;
+			case 2:
+				bundle.putInt(Constants.SORT_MODE, 2);
+				break;
+		}
+
+		fragment.setArguments(bundle);
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// get item count - equal to number of tabs
+		return Constants.LIST_NUMBER_OF_TABS;
+	}
+
+	@Override
+	public int getItemPosition(Object object) {
+		return POSITION_NONE;
+	}
 }
--- Project/src/se/chalmers/krogkollen/utils/Constants.java
+++ Project/src/se/chalmers/krogkollen/utils/Constants.java
@@ -1,25 +1,29 @@
 package se.chalmers.krogkollen.utils;
 
 import se.chalmers.krogkollen.R;
+
 /**
  * A class containing constant values
+ * 
  * @author Jonathan Nilsfors
- *
+ * @author Oskar Karrman
  */
 public class Constants {
 
-    public static int MAP_SHORT_QUEUE = R.drawable.green_marker_bg;
-	public static int MAP_MEDIUM_QUEUE = R.drawable.yellow_marker_bg;
-	public static int MAP_LONG_QUEUE = R.drawable.red_marker_bg;
-	public static int MAP_NO_INFO_QUEUE = R.drawable.gray_marker_bg;
+	public static int			MAP_SHORT_QUEUE			= R.drawable.green_marker_bg;
+	public static int			MAP_MEDIUM_QUEUE		= R.drawable.yellow_marker_bg;
+	public static int			MAP_LONG_QUEUE			= R.drawable.red_marker_bg;
+	public static int			MAP_NO_INFO_QUEUE		= R.drawable.gray_marker_bg;
 
-    public static String ACTIVITY_FROM = "FROM";
+	public static final String	ACTIVITY_FROM			= "FROM";
 
-    public static final String MAP_ACTIVITY_NAME = "MapActivity";
-    public static final String HELP_ACTIVITY_NAME = "HelpActivity";     // Not currently in use but could come in handy
-                                                                        // if the help activity get menu items.
-    public static final String LIST_ACTIVITY_NAME = "ListActivity";
-    public static final String DETAILED_ACTIVITY_NAME = "DetailedActivity";
-    public static final String SEARCH_ACTIVITY_NAME = "SearchActivity";
+	public static final String	MAP_ACTIVITY_NAME		= "MapActivity";
+	public static final String	HELP_ACTIVITY_NAME		= "HelpActivity";
+	public static final String	LIST_ACTIVITY_NAME		= "ListActivity";
+	public static final String	DETAILED_ACTIVITY_NAME	= "DetailedActivity";
+	public static final String	SEARCH_ACTIVITY_NAME	= "SearchActivity";
 
+	public static final String	SORT_MODE				= "SORT_MODE";
+
+	public static final int		LIST_NUMBER_OF_TABS		= 3;
 }
